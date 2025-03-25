ee9f989ada90afa10f7c27407603eb98478f66db
unknown <riyankpatel036@gmail.com>
2025-03-26 04:49:30 +0530

    Create a course selling app using MongoDb

--- week-3/03-mongo/routes/user.js
+++ week-3/03-mongo/routes/user.js
@@ -1,22 +1,70 @@
 const { Router } = require("express");
 const router = Router();
+const { User, Course } = require('../db');
 const userMiddleware = require("../middleware/user");
+const { default: mongoose } = require("mongoose");
 
 // User Routes
-router.post('/signup', (req, res) => {
+router.post('/signup',async (req, res) => {
     // Implement user signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+
+   await User.create({
+        username,
+        password
+    })
+    res.status(200).json({
+        msg: "User Created Successfully"
+    })
 });
 
-router.get('/courses', (req, res) => {
+router.get('/courses', async (req, res) => {
     // Implement listing all courses logic
+const response = await Course.find({})
+
+res.status(200).json({
+    courses: response
 });
 
-router.post('/courses/:courseId', userMiddleware, (req, res) => {
+});
+
+router.post('/courses/:courseId', userMiddleware, async (req, res) => {
     // Implement course purchase logic
+   const courseId = req.params.courseId;
+   const username = req.headers.username;
+  try {
+    await User.updateOne({
+        username: username
+    }, {
+        "$push": {
+            purchasedCourses: courseId
+        }
+    })
+  } catch(e) {
+    console.log(e)
+  };
+  res.status(200).json({
+    msg: "Purchase Complited!"
+  })
 });
 
-router.get('/purchasedCourses', userMiddleware, (req, res) => {
+router.get('/purchasedCourses', userMiddleware, async (req, res) => {
     // Implement fetching purchased courses logic
+   const user = await User.findOne({
+    username: req.headers.username
+   });
+   console.log(user.purchasedCourses);
+   const courses = await Course.find({
+    _id: {
+        "$in": user.purchasedCourses
+    }
+   });
+   res.status(200).json({
+    courses: courses
 });
+});
+
+
 
 module.exports = router
